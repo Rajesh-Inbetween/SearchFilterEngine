d29d8b598dfcb4d69510df9893dc25023acf3acc
Rohith Premachandran <rohith@inbetween.com>
2015-05-15 20:14:28 +0530

    tests populated

--- src/test/java/com/cs/searchfilter/test/PushContentTest.java
+++ src/test/java/com/cs/searchfilter/test/PushContentTest.java
@@ -1,9 +1,21 @@
 package com.cs.searchfilter.test;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.Test;
 
+import com.cs.ce.interactor.incoming.boundary.IInteractor;
 
+@ContextConfiguration
 public class PushContentTest extends AbstractTestNGSpringContextTests {
-
-	
+  
+  @Autowired
+  IInteractor pushContent;
+  
+  @Test
+  public void execute(){
+    pushContent.execute(null);
+  }
+  
 }
--- src/test/java/com/cs/searchfilter/test/SearchContentWithContains.java
+++ src/test/java/com/cs/searchfilter/test/SearchContentWithContains.java
@@ -1,7 +1,21 @@
 package com.cs.searchfilter.test;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.Test;
 
+import com.cs.ce.interactor.incoming.boundary.IInteractor;
+
+@ContextConfiguration
 public class SearchContentWithContains extends AbstractTestNGSpringContextTests  {
 
+  @Autowired
+  IInteractor searchContains;
+  
+  @Test
+  public void execute(){
+    searchContains.execute(null);
+  }
+  
 }
