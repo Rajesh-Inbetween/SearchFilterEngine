c370b3968133873338175c6b018e93d4be20de43
Rohith Premachandran <rohith@inbetween.com>
2015-05-15 18:10:24 +0530

    added spring

--- src/main/java/com/cs/searchfilter/elasticsearch/Elasticsearch.java
+++ src/main/java/com/cs/searchfilter/elasticsearch/Elasticsearch.java
@@ -1,30 +1,30 @@
 package com.cs.searchfilter.elasticsearch;
 
+import javax.annotation.PostConstruct;
+
 import org.elasticsearch.client.Client;
 import org.elasticsearch.node.Node;
 import org.elasticsearch.node.NodeBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-
+@Configuration
 public class Elasticsearch {
   
   private Node node;
 
   private Client client;
   
-  private static Elasticsearch instance;
-  
-  static {
-    instance = new Elasticsearch();
-  }
-  
-  private Elasticsearch()
+  @PostConstruct
+  public void startup()
   {
     node = NodeBuilder.nodeBuilder().node();
     client = node.client();
   }
   
-  public static Client getClient(){
-    return instance.getClient();
+  @Bean
+  public Client client(){
+    return client;
   }
   
   
